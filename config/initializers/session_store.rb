1257758307ed0d15926852cde7b97147efcd5299
Yosei Ito <yousei.itou@gmail.com>
2015-03-15 17:37:05 +0900

    旧ドメイン(.asia)の名前が残っていた箇所を(.netに)変更。

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_events_lmlab_asia_session'
+Rails.application.config.session_store :cookie_store, key: '_events_lmlab_net_session'
