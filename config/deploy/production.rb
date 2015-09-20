9612cc7456e791ac232a04959ce3d73ef9e44dcc
Yosei Ito <yousei.itou@gmail.com>
2015-09-21 07:34:04 +0900

    Adjust settings for deplomnent to new hosts.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,3 @@
-
+set :rails_env, 'production'
+set :deploy_to, '/opt/events.lmlab.net'
 server 'sakura16', user: fetch(:user), roles: %w{app db web}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,3 @@
-
+set :rails_env, 'production'
+set :deploy_to, '/opt/dev.events.lmlab.net'
 server 'sakura15', user: fetch(:user), roles: %w{app db web}
