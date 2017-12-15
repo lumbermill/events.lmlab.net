19a38e1a8fb187ea117c3cb9ae53a5f56648ad27
Yosei Ito <y-itou@lumber-mill.co.jp>
2017-12-15 16:28:17 +0900

    production sakura16 -> sakura23(ubuntu)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :rails_env, 'production'
-set :deploy_to, '/opt/events.lmlab.net'
-server 'sakura16', user: fetch(:user), roles: %w{app db web}
+set :deploy_to, '/var/www/events'
+server 'sakura23', user: fetch(:user), roles: %w{app db web}
